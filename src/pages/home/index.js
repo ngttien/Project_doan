461eb9af29d293791339f10510ac6c337fbf8667
Sonhoccode <magonsilver05@gmail.com>
2025-03-03 06:56:57 +0700

    add

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,68 +1,102 @@
+// import { useState, useEffect } from "react";
 import styles from './home.module.scss';
+import 'react-multi-carousel/lib/styles.css';
 import classNames from 'classnames/bind';
+import Carousel from "react-multi-carousel";
+import saleImg1 from '../../assets/user/images/sale/sale_1.png';
+import saleImg2 from '../../assets/user/images/sale/sale_2.png';
+import saleImg3 from '../../assets/user/images/sale/sale_3.png';
+import saleImg4 from '../../assets/user/images/sale/sale_4.png';
 
 const cx = classNames.bind(styles);
-function Home() {
+const Home = () => {
+    //  Dữ liệu mẫu cập nhật backend
+    // const [sliderItems, setSliderItems] = useState([]);
+    // useEffect(() => {
+    //     // Giả sử API của bạn trả về danh sách ảnh từ Firebase
+    //     fetch("http://localhost:5000/api/sliders") // Đổi thành API của bạn
+    //         .then((res) => res.json())
+    //         .then((data) => setSliderItems(data))
+    //         .catch((err) => console.error(err));
+    // }, []);
+    const responsive = {
+        superLargeDesktop: {
+          breakpoint: { max: 4000, min: 3000 },
+          items: 2
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
+
+      const sliderItems = [
+        {
+            bgImg: saleImg1,
+        },
+        {
+            bgImg: saleImg2,
+        },
+        {
+            bgImg: saleImg3,
+        },
+        {
+            bgImg: saleImg4,
+        }
+      ];
+
     return ( 
         <home className={cx("home-container")}>
             <div className={cx('container')}>
-                <div className={cx('row')}>
-                        <div className={cx('home')}>
-                        <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p> <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p> <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p> <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p> <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p> <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p> <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p> <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p> <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p>
-                            <p>2</p> 
+                <div className={cx('home')}>
+                    <div className={cx('frame_intro')}>
+                        <div className={cx('intro')}>
+                            <p>Giới thiệu</p>
+                        </div>
+                        <div className={cx('contact')}>
+                            <h3>Liên hệ chúng tôi tại</h3>
+                            <ul>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={cx('sale_slider')}>
+                        <Carousel 
+                            responsive={responsive} 
+                            className={cx('slider')} 
+                            infinite={true}
+                            autoPlay={true}
+                            autoPlaySpeed={1500}
+                            >
+                            {/* Mẫu cập nhật ảnh qua backend
+                            {sliderItems.map((item, index) => (
+                                <div 
+                                    key={index} 
+                                    className={cx('slider_items')}
+                                    style={{ backgroundImage: `url(${item.imageUrl})` }}>
+                                </div>
+                            ))} */}
+                            {/* ảnh Mẫu */}
+                            
+                            {sliderItems.map((item, key) => (
+                                <div 
+                                className={cx('slider_items')}
+                                style={{backgroundImage: `url(${item.bgImg})`}}
+                                onClick={() => window.location.href = '/#'}
+                                key={key}
+                            ></div>
+                            ))}
+                            
+                        
+                        </Carousel>
                     </div>
                 </div>
             </div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,18 @@ import Home from '~/pages/home/index';
 import Menu from '~/pages/menu/index';
 import Contact from '~/pages/contact/index';
 import Login from '~/pages/login/index';
+import Booking from '~/pages/booking/index';
+import Sale from '~/pages/sale/index';
+import Intro from '~/pages/intro/index';
+
 
 const config = [
     { path: '/', component: Home },
     { path: '/menu', component: Menu },
     { path: '/contact', component: Contact },
     { path: '/login', component: Login, Lg: null },
+    { path: '/booking', component: Booking},
+    { path: '/sale', component: Sale},
+    { path: '/intro', component: Intro}
 ];
 export default config;
